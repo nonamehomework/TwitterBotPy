21a3536f04907f22a97359f5e03e31c24326e275
lnsf <lnsf03@gmail.com>
2019-08-22 14:29:07 +0900

    :sparkles: Change connecting behavior

--- botkun/lib/markov.py
+++ botkun/lib/markov.py
@@ -22,7 +22,8 @@ def parse_string(string: str, mecab: Tagger) -> [str]:
 
 def create_word_block(words: [str]) -> [[str]]:
     word_block = []
-    words.insert(0, "")
+    words.insert(0, "")  # head of string
+    words.append("")  # last of string
     for i in range(len(words) - 2):
         word_block.append([words[i], words[i + 1], words[i + 2]])
 
@@ -32,7 +33,12 @@ def create_word_block(words: [str]) -> [[str]]:
 def connect_word_blocks(word_blocks: [[str]], connected_indexes=None) -> [int]:
     if connected_indexes is None:
         connected_indexes = []
-    last_word = "" if len(connected_indexes) == 0 else word_blocks[connected_indexes[-1]][2]
+
+    last_word = ""
+    if len(connected_indexes) != 0:
+        last_word = word_blocks[connected_indexes[-1]][2]
+        if last_word == "":  # in case last word is end of string
+            return connected_indexes
 
     next_candidate = []
     for index, word_block in enumerate(word_blocks):
